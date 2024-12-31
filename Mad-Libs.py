9a759a9882f9be730bbf2b858cefeea5b66536bd
sebarouco <seba.rouco.oliva@gmail.com>
2024-12-30 21:01:46 -0300

    Mad Libs

---
+++ Mad-Libs.py
@@ -0,0 +1,34 @@
+adjective = input("Enter a adjective: ")
+adjective2 = input("Enter a adjective: ")
+noun = input("Enter a noun: ")
+noun2 = input("Enter a noun: ")
+plural_noun = input("Enter a plural noun: ")
+game = input("Enter a name of a game: ")
+plural_noun2 = input("Enter a plural noun: ")
+ing_verb = input("Enter a verb ending in -ing: ")
+ing_verb2 = input("Enter a verb ending in -ing: ")
+plural_noun3 = input("Enter a plural noun: ")
+ing_verb3 = input("Enter a verb ending in -ing: ")
+noun3 = input("Enter a noun: ")
+plant = input("Enter a name of a plant: ")
+body_part = input("Enter the name of a body part: ")
+place = input("Enter a name of a place: ")
+ing_verb4 = input("Enter a verb ending in -ing: ")
+adjective3 = input("Enter a adjective: ")
+number = input("Enter a number: ")
+plural_noun4 = input("Enter a plural noun: ")
+
+story = ("A vacation is when you take a break or take a trip somewhere " + adjective + " place with your " +
+adjective2 + " family. Usually you go to some place that is near a/an " + noun + " or up on a/an "+
+noun2 + ". A good vacation place is one where you can " + plural_noun + " or play " + game
++ " or go hunting for " + plural_noun2 + ". I like to spend my time "+ ing_verb + " or " + ing_verb2
++". When parents go on a vacation, they spend their time eating three "+ plural_noun3 +
+" a day, and fathers play Baseball, and mothers sit around " + ing_verb3
++ ". Last summer, my little sister fell in a/an " + noun3 + " and got poison " + plant
++ " all over his " + body_part + ". My family is going to go to the " + place
++ ", and I will practice " + ing_verb4 +
+". Parents need vacations more than kids because parents are always very " + 
+adjective3 + " and because they have to work " + number
++ " hours every day all year making enough " + plural_noun4 + " to pay for the vacation.")
+
+print(story)
